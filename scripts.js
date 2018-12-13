df6333506245abf5fdeda207a05d2fe487a2cf59
sbddesign <stephen@d.elor.me>
2018-12-13 15:01:46 -0500

    Factored audio in to calculation size

--- scripts.js
+++ scripts.js
@@ -4,26 +4,37 @@
     var desiredSize     = document.getElementById("desiredSize"),
         videoMinutes    = document.getElementById("videoMinutes"),
         videoSeconds    = document.getElementById("videoSeconds"),
+        audioInVideo    = document.getElementById("audioInVideo"),
         desiredSizeInMegabits,
         videoLengthInSeconds,
-        result;
+        result,
+        audioMessage;
 
 
-    function framecruncher(desiredSize, videoMinutes, videoSeconds) {
+    function framecruncher(desiredSize, videoMinutes, videoSeconds, audioInVideo) {
 
         desiredSizeInMegabits = desiredSize*8*1024;
         videoLengthInSeconds = (parseFloat(videoMinutes) * 60) + parseFloat(videoSeconds);
-        result = desiredSizeInMegabits / videoLengthInSeconds;
 
-        document.getElementById("result").innerHTML = 'Use a maximum bitrate of ' + result.toFixed(1) + ' Mbps';
+        if(audioInVideo === true) {
+            // Assuming 320Kbps audio, then converting to Mbps
+            audioSize = (videoLengthInSeconds * 320)/1024;
+            audioMessage = '<br />(Assuming your audio bitrate is 320 Kbps).';
+        } else {
+            audioSize = 0;
+            audioMessage = '';
+        }
+        result = (desiredSizeInMegabits - audioSize) / videoLengthInSeconds;
+
+        document.getElementById("result").innerHTML = 'Use a maximum video bitrate of <strong>' + result.toFixed(1) + ' Mbps.</strong>' + audioMessage;
 
     }
 
-    framecruncher(desiredSize.value, videoMinutes.value, videoSeconds.value);
+    framecruncher(desiredSize.value, videoMinutes.value, videoSeconds.value, audioInVideo.checked);
 
 
     document.getElementById("submit").addEventListener("click", function(){
-        framecruncher(desiredSize.value, videoMinutes.value, videoSeconds.value);
+        framecruncher(desiredSize.value, videoMinutes.value, videoSeconds.value, audioInVideo.checked);
     });
 
 })();
